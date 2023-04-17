a07f7ae167814b61d0fc06d0f9d55d9b5fe3b461
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2023-04-17 21:28:48 +0800

    created navbar in the navbar-item.blade made on Mon Apr 17 21:28:48 +08 2023

--- resources/views/layout/navbar-item.blade.php
+++ resources/views/layout/navbar-item.blade.php
@@ -1,4 +1,5 @@
-<div>
-    {{$href}}
-
-</div>
+<li>
+    <a href="{{ $href }}" class="text-xs font-medium text-dark py-2 flex hover:text-primary dark:text-white lg:inline-flex lg:ml-6 xl:ml-12">
+        {{ $slot }}
+    </a>
+</li>
