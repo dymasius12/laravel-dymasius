734956f4f35f4a6c919598f01980378a61e84414
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2023-05-18 23:06:18 +0800

    Added Call To Action Section made on Thu May 18 23:06:18 +08 2023

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -7,15 +7,15 @@
                 <div class="flex items-center -mx-3 sm:-mx-4">
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
                         <div class="py-3 sm:py-4">
-                            <img src="{{ url('/img/me1.png') }}" alt="" class="rounded-2xl w-full" />
+                            <img src="{{ url('/img/me1.jpeg') }}" alt="" class="rounded-2xl w-full" />
                         </div>
                         <div class="py-3 sm:py-4">
-                            <img src="{{ url('/img/me3.png') }}" alt="" class="rounded-2xl w-full" />
+                            <img src="{{ url('/img/me3.jpeg') }}" alt="" class="rounded-2xl w-full" />
                         </div>
                     </div>
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
                         <div class="my-4 relative z-10">
-                            <img src="{{ url('/img/me2.png') }}" alt="" class="rounded-2xl w-full" />
+                            <img src="{{ url('/img/me2.jpeg') }}" alt="" class="rounded-2xl w-full" />
                             <x-about-dots></x-about-dots>
                         </div>
                     </div>
@@ -29,10 +29,10 @@
                             with hard work. Collective hard work and small success"
                         </blockquote>
                     </span>
-                    <h2 class="font-bold text-3xl sm:text-4xl dark:text-grey-200 mb-8">
+                    <h2 class="font-bold text-3xl sm:text-4xl dark:text-white mb-8">
                         About me:
                     </h2>
-                    <p class="text-base dark:text-gray-400 mb-8">
+                    <p class="text-base dark:text-white mb-8">
                         I have been working as a professional software developer for 3+ years. <br />
                         During my career I have worked on various types of projects with various technologies, such as:
                         <br />
@@ -40,7 +40,7 @@
                         PostGreSQL, Apache, MongoDB, etc.<br />
                         I also learn some stacks, such as: MERN, MEVN, etc.
                     </p>
-                    <p class="text-base dark:text-gray-400 mb-8">
+                    <p class="text-base dark:text-white mb-8">
                         In my freetime, I create a youtube channel to share about education and University <br />
                         I have interviewed a couple of famous people, starting from a CEO to inspirational figures.
                         <br />
--- resources/views/components/home/learn-php.blade.php
+++ resources/views/components/home/learn-php.blade.php
@@ -1,3 +1,17 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<x-call-to-action>
+    <x-slot:title>
+      <h4 class="text-white text-base font-semibold mb-2">
+        Check more about myself in Youtube and Github
+      </h4>
+      <h2 class=" text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0 ">
+        I Share About <br class="hidden xs:block"/>
+        Programming or University
+      </h2>
+    </x-slot>
+      <x-button-link href="https://github.com/dymasius12" variant="dark" class="mr-3" target="_blank">
+        View my channel
+      </x-button-link>
+      <x-button-link href="https://www.youtube.com/getkampus" variant="red" target="_blank">
+        Watch on YouTube
+      </x-button-link>
+  </x-call-to-action>
